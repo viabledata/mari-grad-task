43ae5b224c9a1ae8b3d6f1cc9bd928430f646df6
Mari <marygt182@gmail.com>
2024-04-29 17:34:58 +0100

    Add parse object to read excel file

--- models/validation.py
+++ models/validation.py
@@ -1,27 +1,34 @@
 from datetime import datetime, date, time
-
+from typing import Optional
 from pydantic import BaseModel, field_validator, Field
 
 
-class Person(BaseModel):
-    id: int
-    name: str
-    dob: date
-    time_in: time
-    membership_no: int
-    valid_from: date
-    valid_to: date
-    gender: str
-    researcher: str
+class Person_model(BaseModel):
+    name: Optional[str] = None
+    dob: Optional[date] = None
+    time_in: Optional[time] = None
+    membership_no: Optional[int] = None
+    valid_from: Optional[date] = None
+    valid_to: Optional[date] = None
+    librarian: Optional[str] = None
+    gender: Optional[str] = None
+    researcher: Optional[str] = None
 
-    @field_validator("dob", "valid_from", "valid_to")
+    @field_validator("dob")
     def check_valid_date(cls, input_date):
         if not isinstance(input_date, date):
             raise ValueError("Must be a valid date")
+        print(input_date)
         return input_date
 
     @field_validator("time_in")
     def check_valid_time(cls, input_time):
         if not isinstance(input_time, time):
             raise ValueError("Must be a valid time")
+        print(input_time)
         return input_time
+
+class Config:
+    title = "Person_model"
+    # allow_population_by_field_name = True
+    orm_mode = True
