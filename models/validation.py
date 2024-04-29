4cfa2071eca55e6d2b8cea02a2741f92c2d19b27
Mari <marygt182@gmail.com>
2024-04-29 15:17:18 +0100

    Add field validators for date and time

--- models/validation.py
+++ models/validation.py
@@ -1,6 +1,6 @@
 from datetime import datetime, date, time
 
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator, Field
 
 
 class Person(BaseModel):
@@ -14,3 +14,14 @@ class Person(BaseModel):
     gender: str
     researcher: str
 
+    @field_validator("dob", "valid_from", "valid_to")
+    def check_valid_date(cls, input_date):
+        if not isinstance(input_date, date):
+            raise ValueError("Must be a valid date")
+        return input_date
+
+    @field_validator("time_in")
+    def check_valid_time(cls, input_time):
+        if not isinstance(input_time, time):
+            raise ValueError("Must be a valid time")
+        return input_time
