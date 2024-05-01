c60302de4766f9aa4af8a46f4097f5d739164ff4
Mari <marygt182@gmail.com>
2024-05-01 16:16:07 +0100

    Make date validator accept different formats and parse them all to datetime format

--- models/validation.py
+++ models/validation.py
@@ -1,31 +1,39 @@
 from datetime import datetime, date, time
 from typing import Optional
+import pandas
 from pydantic import BaseModel, validator, Field
+from dateutil import parser
+from pydantic.class_validators import Union
 
 
 class Person_model(BaseModel):
-    name: Optional[str] = None
-    dob: Optional[date] = None
-    time_in: Optional[time] = None
-    membership_no: Optional[int] = None
-    valid_from: Optional[date] = None
-    valid_to: Optional[date] = None
-    librarian: Optional[str] = None
-    gender: Optional[str] = None
-    researcher: Optional[str] = None
+    name: Optional[str]
+    dob: Union[datetime, str]
+    time_in: Optional[time]
+    membership_no: Optional[int]
+    valid_from: Union[datetime, str]
+    valid_to: Union[datetime, str]
+    librarian: Optional[str]
+    gender: Optional[str]
+    researcher: Optional[str]
 
-    @validator("dob")
+    @validator('dob', 'valid_from', 'valid_to')
     def check_valid_date(cls, input_date):
-        if not isinstance(input_date, date):
-            raise ValueError("Must be a valid date")
-        # print(f"valdation dob: {input_date}")
+        if isinstance(input_date, str):
+            new_date = parser.parse(input_date)
+            print(type(new_date))
+            return new_date
+        if isinstance(input_date, pandas.Timestamp):
+            new_date = input_date.to_pydatetime()
+            print(type(new_date))
+            return new_date
+        print(type(input_date))
         return input_date
 
-    @validator("time_in")
+    @validator('time_in')
     def check_valid_time(cls, input_time):
         if not isinstance(input_time, time):
             raise ValueError("Must be a valid time")
-        # print(f"validation time_in: {input_time}")
         return input_time
 
 
